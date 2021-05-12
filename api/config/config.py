89ca9cb4da6e0c26fd297fa944944919d27f251d
Klila Ayed <klilaayed195@gmail.com>
2021-05-12 19:44:11 +0200

    First commit

--- api/config/config.py
+++ api/config/config.py
@@ -10,13 +10,6 @@ class ProductionConfig(Config):
     JWT_SECRET_KEY = 'JWT-SECRET'
     SECRET_KEY= 'SECRET-KEY'
     SECURITY_PASSWORD_SALT= 'SECRET-KEY-PASSWORD'
-    MAIL_DEFAULT_SENDER= '<mail_sender>'
-    MAIL_SERVER= '<mail_smtp_host>'
-    MAIL_PORT= '<mail_port>'
-    MAIL_USERNAME= '<mail_username>'
-    MAIL_PASSWORD= '<mail_password>'
-    MAIL_USE_TLS= False
-    MAIL_USE_SSL= True
     UPLOAD_FOLDER= '<upload_folder>'
 
 
@@ -27,13 +20,6 @@ class DevelopmentConfig(Config):
     JWT_SECRET_KEY = 'JWT-SECRET'
     SECRET_KEY= 'SECRET-KEY'
     SECURITY_PASSWORD_SALT= 'SECRET-KEY-PASSWORD'
-    MAIL_DEFAULT_SENDER= '<mail_sender>'
-    MAIL_SERVER= '<mail_smtp_host>'
-    MAIL_PORT= '<mail_port>'
-    MAIL_USERNAME= '<mail_username>'
-    MAIL_PASSWORD= '<mail_password>'
-    MAIL_USE_TLS= False
-    MAIL_USE_SSL= True
     UPLOAD_FOLDER= '<upload_folder>'
 
 
@@ -45,11 +31,4 @@ class TestingConfig(Config):
     JWT_SECRET_KEY = 'JWT-SECRET'
     SECRET_KEY= 'SECRET-KEY'
     SECURITY_PASSWORD_SALT= 'SECRET-KEY-PASSWORD'
-    MAIL_DEFAULT_SENDER= '<mail_sender>'
-    MAIL_SERVER= '<mail_smtp_host>'
-    MAIL_PORT= '<mail_port>'
-    MAIL_USERNAME= '<mail_username>'
-    MAIL_PASSWORD= '<mail_password>'
-    MAIL_USE_TLS= False
-    MAIL_USE_SSL= True
     UPLOAD_FOLDER= '<upload_folder>'
